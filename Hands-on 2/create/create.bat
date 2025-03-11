124e2764d10980c1ee915888117cd0154f9a5d49
Luis Alejandro Sánchez Brizio <luis.sanchez9105@alumnos.udg.mx>
2025-03-10 18:16:28 -0600

    Update create.bat

--- Hands-on 2/create/create.bat
+++ Hands-on 2/create/create.bat
@@ -1,1 +1,31 @@
+@echo off
+echo Creando el archivo de texto "mytext.txt" con el mensaje "Hola Mundo"...
+echo Hola Mundo > mytext.txt
 
+echo.
+echo Mostrando el contenido de "mytext.txt":
+type mytext.txt
+
+echo.
+echo Creando el subdirectorio "backup"...
+mkdir backup
+
+echo.
+echo Copiando "mytext.txt" al subdirectorio "backup"...
+copy mytext.txt backup\
+
+echo.
+echo Listando el contenido del subdirectorio "backup":
+dir backup
+
+echo.
+echo Eliminando "mytext.txt" dentro de "backup"...
+del backup\mytext.txt
+
+echo.
+echo Eliminando el subdirectorio "backup"...
+rmdir backup
+
+echo.
+echo Proceso completado.
+pause
